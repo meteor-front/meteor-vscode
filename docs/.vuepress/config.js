04885741fe279555620af2bb4d70c31e70002422
jiaolong1021 <shenjiaolong1021@163.com>
2021-03-22 13:20:13 +0800

    remove unuse

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,45 +1,20 @@
 module.exports = {
-  title: 'Vetur',
-  description: 'Vue tooling for VS Code.',
-  base: '/vetur/',
+  title: 'Meteor',
+  description: 'Meteor',
+  base: '/Meteor/',
   markdown: {
     linkify: true
   },
   themeConfig: {
-    repo: 'vuejs/vetur',
+    repo: 'Meteor',
     editLinks: true,
     docsDir: 'docs',
     nav: [
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Reference', link: '/reference/' },
-      { text: 'FAQ', link: '/guide/FAQ' },
-      { text: 'Roadmap', link: 'https://github.com/vuejs/vetur/issues/873' },
-      { text: 'Credits', link: '/credits' },
-      { text: 'Contribution Guide', link: 'https://github.com/vuejs/vetur/wiki#contribution-guide' }
+      { text: '/', link: '/' }
     ],
     sidebar: {
-      '/guide/': [
-        '',
-        'setup',
-        {
-          title: 'Features',
-          collapsable: false,
-          children: [
-            'highlighting',
-            'snippet',
-            'emmet',
-            'linting-error',
-            'formatting',
-            'intellisense',
-            'debugging',
-            'component-data',
-            'interpolation',
-            'global-components'
-          ]
-        },
-        'FAQ'
-      ],
-      '/reference/': ['', 'tsconfig']
+      '/': [
+      ]
     }
   }
 };
