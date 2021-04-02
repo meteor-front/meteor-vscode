acd631882dbc84c2bcccf47abfc6f870646fa400
jiaolong1021 <shenjiaolong1021@163.com>
2021-04-02 16:50:06 +0800

    version 3.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,33 @@
 # CHANGELOG FOR METEOR
+
+# 扩展功能
+插件meteor的宗旨是提高开发效率，一切能够提高效率的功能，meteor都是欢迎的。<br>
+目前meteor中扩展的功能主要有以下部分：<br>
+![扩展功能](https://www.80fight.cn/vscode/componentExtend.png)
+## vue方法生成
+快捷键 （alt + shift + enter）
+![newMethod.gif](https://www.80fight.cn/vscode/newMethod.gif)
+## api接口生成
+在vscode命令行执行 `meteor:api`
+![apiCreate.gif](https://www.80fight.cn/vscode/apiCreate.gif)
+## swagger生成接口
+在vscode命令行执行 `meteor:swagger`。可根据swagger一次性全部生成，也可以根据某个接口名称单个生成。
+![swaggerCreate.gif](https://www.80fight.cn/vscode/swaggerCreate.gif)
+## vscode右上角进入官网
+![goOfficial.gif](https://www.80fight.cn/vscode/goOfficial.gif)
+## 完成项功能增强
+#### 一、import导入提示<br>
+![goOfficial.gif](https://www.80fight.cn/vscodeImgs/completionItemImport.gif)
+相关配置参数：<br>
+[componentNamingRule]() 指定组件生成规则，有`kebabCase`、`camelCase`、`CamelCase`<br>
+[pathAlias]() 指定别名对应的路径<br>
+#### 二、 element代码块提示，属性提示
+![completionItemElement.gif](https://www.80fight.cn/vscodeImgs/completionItemElement.gif)
+#### 三、工程内组件引入
+![autoImportComponent](https://www.80fight.cn/vscodeImgs/autoImportComponent.gif)
+#### 四、自动导入文件
+![autoImport](https://www.80fight.cn/vscodeImgs/autoImport.gif)
+#### 五、函数自动完成
+![functionAutoComplete](https://www.80fight.cn/vscodeImgs/functionAutoComplete.gif)
+## 块选择功能
+![blockSelect](https://www.80fight.cn/vscodeImgs/blockSelect.gif)
--- client/MeteorMain.ts
+++ client/MeteorMain.ts
@@ -28,9 +28,9 @@ export function activate(context: vscode.ExtensionContext) {
   const meteor = new Meteor(context)
   
   // 为标签、属性提示提供自动完成功能, 关闭标签功能
-  let completionDisposible = vscode.languages.registerCompletionItemProvider(['vue', 'javascript', 'html', 'wxml'], meteor.completionItemProvider, '' ,':', '<', '"', "'", '/', '@', '(', '>', '{');
+  vscode.languages.registerCompletionItemProvider(['vue', 'javascript', 'html', 'wxml'], meteor.completionItemProvider, '' ,':', '<', '"', "'", '/', '@', '(', '>', '{');
   // 函数补全函数
-  let functionCompletionDisposable = vscode.commands.registerCommand('meteor.functionCompletion', () => {
+  vscode.commands.registerCommand('meteor.functionCompletion', () => {
     let editor = vscode.window.activeTextEditor;
     if (!editor) { return; }
     let txt = editor.document.lineAt(editor.selection.anchor.line).text;
@@ -42,39 +42,39 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
   // 代码块选择
-  let blockSelectDisposable = vscode.commands.registerCommand('meteor.blockSelect', () => {
+  vscode.commands.registerCommand('meteor.blockSelect', () => {
       meteor.block.select()
   });
   // 删除处理函数
-  let deleteCompleteDisposable = vscode.commands.registerCommand('meteor.deleteComplete', () => {
+  vscode.commands.registerCommand('meteor.deleteComplete', () => {
     meteor.backSpace.deleteComplete()
   });
   // 打开官网
-	let openOfficialDisposable = vscode.commands.registerCommand('meteor.openOfficial', (uri) => {
+	vscode.commands.registerCommand('meteor.openOfficial', (uri) => {
 		open(url.official);
 	});
   // swagger生成api
-	let meteorSwaggerDispoable = vscode.commands.registerCommand('meteor.swagger', async () => {
+	vscode.commands.registerCommand('meteor.swagger', async () => {
 		meteor.swagger.generate()
 	});
   // 页面生成
-	let newPageDisposable = vscode.commands.registerCommand('meteor.newPage', (uri) => {
+	vscode.commands.registerCommand('meteor.newPage', (uri) => {
 		meteor.newPage.showQuickPick(context, uri)
 	});
   // 同步数据
-	let meteorSyncDispoable = vscode.commands.registerCommand('meteor.sync', async () => {
+	vscode.commands.registerCommand('meteor.sync', async () => {
 		meteor.sync();
 	});
   // 上传页面/组件
-	let uploadDisposable = vscode.commands.registerCommand('meteor.upload', (activeTab) => {
+	vscode.commands.registerCommand('meteor.upload', (activeTab) => {
 		UploadPanel.createOrShow(context.extensionPath, activeTab);
 	});
   // 新建工程
-	let newProjectDisposable = vscode.commands.registerCommand('meteor.newProject', () => {
+	vscode.commands.registerCommand('meteor.newProject', () => {
 		NewProjectPanel.createOrShow(context.extensionPath);
 	});
   // 打开工程目录
-	let openProjectDisposable = vscode.commands.registerCommand("meteor.openProject", (node: string | any) => {
+	vscode.commands.registerCommand("meteor.openProject", (node: string | any) => {
 		let projectDir = NewProjectPanel.projectDir;
 		if (os.platform().includes('win')) {
 			projectDir = '/' + projectDir.replace(/\\/gi, '/');
@@ -84,21 +84,17 @@ export function activate(context: vscode.ExtensionContext) {
 					value => vscode.window.showInformationMessage("打开工程失败！"));
 	});
   // 生成组件
-	let newComponentDisposable = vscode.commands.registerCommand('meteor.newComponent', (uri) => {
+	vscode.commands.registerCommand('meteor.newComponent', (uri) => {
 		meteor.newPage.showComponentQuickPick(context, uri);
 	});
   // 接口生成
-	let apiDisposable = vscode.commands.registerCommand('meteor.api', (uri) => {
+	vscode.commands.registerCommand('meteor.api', (uri) => {
 		meteor.newPage.api(context, uri);
 	});
   // 到达定义函数
-  let meteorDefinition = vscode.languages.registerDefinitionProvider(['vue', 'javascript', 'html'], new MeteorDefinitionProvider());
-  let registrationHover = vscode.languages.registerHoverProvider('vue', new DocumentHoverProvider);
-  let jsCompletion = vscode.languages.registerCompletionItemProvider(['javascript', 'html', 'vue'], new JsCompletionItemProvider(), '.', '(');
-  
-  context.subscriptions.push(completionDisposible, functionCompletionDisposable, blockSelectDisposable, deleteCompleteDisposable, openOfficialDisposable, meteorSwaggerDispoable, 
-    newPageDisposable, meteorSyncDispoable, uploadDisposable, newProjectDisposable, openProjectDisposable, newComponentDisposable, apiDisposable, meteorDefinition,
-    registrationHover, jsCompletion)
+  vscode.languages.registerDefinitionProvider(['vue', 'javascript', 'html'], new MeteorDefinitionProvider());
+  vscode.languages.registerHoverProvider('vue', new DocumentHoverProvider);
+  vscode.languages.registerCompletionItemProvider(['javascript', 'html', 'vue'], new JsCompletionItemProvider(), '.', '(');
 
 	// 服务器用node实现
 	let serverModule = context.asAbsolutePath(
