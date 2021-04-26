97d68743f90395b812c35c875368f3cd79684c98
jiaolong1021 <shenjiaolong1021@163.com>
2021-04-26 22:34:35 +0800

    version 3.4.0

--- client/meteor/functions/swagger.ts
+++ client/meteor/functions/swagger.ts
@@ -605,7 +605,7 @@ ${this.meteor.tabSpace}}`;
     while(currentLine < count) {
       let text = editor.document.lineAt(currentLine).text
       if (new RegExp(`\\s*...mapActions\\(\\s*'\\s*\\w*\\s*'\\s*,\\s*\\[.*\\]\\s*\\)\\s*,\\s*`, 'gi').test(text)) {
-        if (new RegExp(`\\s*...mapActions\\(\\s*'\\s*${fileName}\\s*'\\s*,\\s*\\[.*\\]\\s*\\)\\s*,\\s*`, 'gi').test(text)) {
+        if (new RegExp(`\\s*...mapActions\\(\\s*'\\s*${fileName.replace(/(.*)\..*/, '$1')}\\s*'\\s*,\\s*\\[.*\\]\\s*\\)\\s*,\\s*`, 'gi').test(text)) {
           // 已导入文件，判断接口是否导入
           if (!new RegExp(`\\[.*${apiParams.name}.*\\]`, 'gi').test(text)) {
             insertList.push([new Position(currentLine, text.replace(/\s*\].*/gi, '').length), `, '${apiParams.name}' `])
