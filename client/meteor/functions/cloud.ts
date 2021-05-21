7c0b313b8d4fa13122c190e9a17d1d2ae700cfb6
jiaolong1021 <shenjiaolong1021@163.com>
2021-05-21 11:06:44 +0800

    version 3.5.1

--- client/meteor/functions/cloud.ts
+++ client/meteor/functions/cloud.ts
@@ -20,6 +20,8 @@ export default class Cloud {
   private configUrl: string = ''
   private image: string = ''
   private serviceVo: any = {}
+  private lastLogin: number = 0
+  private isSameEnv: boolean = true
 
   constructor(meteor: Meteor) {
     this.meteor = meteor
@@ -45,6 +47,7 @@ export default class Cloud {
       }
     }
     this.config = config
+    this.isSameEnv = (this.envId === this.config.envId)
     this.envId = this.config.envId
 
     const quickPick = window.createQuickPick()
@@ -73,7 +76,9 @@ export default class Cloud {
     // 选中选项
     quickPick.onDidChangeSelection((selection) => {
       if (selection[0] && selection[0].label) {
-        this.cloudHandle(selection[0].label)
+        this.cloudHandle(selection[0].label).catch((err) => {
+          window.showInformationMessage('抱歉，网络不给力！')
+        })
       }
       quickPick.hide()
     })
@@ -111,114 +116,125 @@ export default class Cloud {
     }
     // 1. 登录获取username
     let url = this.config.url || this.url
-    let res = await axios.post(`${url}/api/login`, {
-      username: this.config.username || this.username,
-      password: this.config.password || this.password
-    })
-    if (res.data.data && res.data.data.token) {
-      this.token = res.data.data.token
-      // 获取组id
-      res = await axios.post(`${this.url}/api/environment/envDetails`, {
-        envId: this.config.envId,
-        pageNum: 1,
-        pageSize: 9999
-      }, {
-        headers: {
-          token: this.token
-        }
+    let res: any = {}
+    // 有token，两小时内部重复登录
+    if (!(this.lastLogin > 0 && (new Date().getTime() < this.lastLogin + 7200000) && this.token)) {
+      res = await axios.post(`${url}/api/login`, {
+        username: this.config.username || this.username,
+        password: this.config.password || this.password
       })
-      if (res.data.data && res.data.data.apps && res.data.data.apps.content) {
-        for (let i = 0; i < res.data.data.apps.content.length; i++) {
-          const group = res.data.data.apps.content[i];
-          if (group.serviceName === this.serviceName) {
-            this.groupId = group.groupId
-            this.image = group.serviceVo.image.replace(/:.*/gi, '')
-            this.serviceVo = group.serviceVo
-            break
+      this.lastLogin = new Date().getTime()
+    }
+    if ((res.data && res.data.data && res.data.data.token) || this.token) {
+      if (res.data) {
+        this.token = res.data.data.token
+      }
+      // 切换环境则重新获取环境信息
+      if (!this.isSameEnv) {
+        res = await axios.post(`${this.url}/api/environment/envDetails`, {
+          envId: this.config.envId,
+          pageNum: 1,
+          pageSize: 9999
+        }, {
+          headers: {
+            token: this.token
+          }
+        })
+        if (res.data.data && res.data.data.apps && res.data.data.apps.content) {
+          for (let i = 0; i < res.data.data.apps.content.length; i++) {
+            const group = res.data.data.apps.content[i];
+            if (group.serviceName === this.serviceName) {
+              this.groupId = group.groupId
+              this.image = group.serviceVo.image.replace(/:.*/gi, '')
+              this.serviceVo = group.serviceVo
+              break
+            }
           }
         }
-        if (this.groupId) {
-          if (mode === '重启') {
-            res = await axios.delete(`${this.url}/api/environments/${this.envId}/groups/${this.groupId}/services/${this.serviceName}/reboot?deploymentName=${this.serviceName}`, {
+      }
+      if (this.groupId) {
+        if (mode === '重启') {
+          res = await axios.delete(`${this.url}/api/environments/${this.envId}/groups/${this.groupId}/services/${this.serviceName}/reboot?deploymentName=${this.serviceName}`, {
+            headers: {
+              token: this.token
+            }
+          })
+          if (res.data && res.data.code === '0') {
+            window.showInformationMessage('重启成功！')
+          } else {
+            window.showInformationMessage('重启失败，请重试！')
+          }
+        } else if (mode === '升级') {
+          res = await axios.get(`${this.url}/api/harbor/images/${this.image}/tags?environment=${this.envId}`, {
+            headers: {
+              token: this.token
+            }
+          })
+          if (res.data.data && res.data.data.length > 0) {
+            this.serviceVo.image = this.image + ':' + res.data.data[0].name
+            res = await axios.put(`${this.url}/api/environments/${this.envId}/groups/${this.groupId}/services/${this.serviceName}/upgrade?upgradeType=roll&autoUpgrade=false`, this.serviceVo, {
               headers: {
                 token: this.token
               }
             })
             if (res.data && res.data.code === '0') {
-              window.showInformationMessage('重启成功！')
+              window.showInformationMessage('升级成功，系统正在重启中...')
             } else {
-              window.showInformationMessage('重启失败，请重试！')
+              window.showInformationMessage('升级失败，请重试！')
             }
-          } else if (mode === '升级') {
-            res = await axios.get(`${this.url}/api/harbor/images/${this.image}/tags?environment=${this.envId}`, {
+          }
+        } else if (mode === '更新配置集') {
+          let data: any = {}
+          try {
+            let rootPath = path.join(getWorkspaceRoot(''), this.configUrl || 'config')
+            const dirs = fs.readdirSync(rootPath)
+            for (let i = 0; i < dirs.length; i++) {
+              const dir = dirs[i];
+              if (!(dir.charAt(0) === '.')) {
+                let stat = fs.statSync(path.join(rootPath, dir))
+                if (stat.isFile()) {
+                  const file = fs.readFileSync(path.join(rootPath, dir), 'utf-8')
+                  data[dir] = file
+                }
+              }
+            }
+          } catch (error) {
+          }
+          if (Object.keys(data).length > 0) {
+            res = await axios.put(`${this.url}/api/environments/${this.envId}/configmaps/hs-screen`, {
+              environmentId: this.envId,
+              id: this.serviceName,
+              data: data,
+              currentVersion: '1'
+            }, {
               headers: {
                 token: this.token
               }
             })
-            if (res.data.data && res.data.data.length > 0) {
-              this.serviceVo.image = this.image + ':' + res.data.data[0].name
-              res = await axios.put(`${this.url}/api/environments/${this.envId}/groups/${this.groupId}/services/${this.serviceName}/upgrade?upgradeType=roll&autoUpgrade=false`, this.serviceVo, {
-                headers: {
-                  token: this.token
-                }
+            if (res.data && res.data.code === '0') {
+              window.showInformationMessage('更新配置信息成功！')
+              this.cloudHandle('重启').catch((err) => {
+                window.showInformationMessage('抱歉，网络不给力！')
               })
-              if (res.data && res.data.code === '0') {
-                window.showInformationMessage('升级成功，系统正在重启中...')
-              } else {
-                window.showInformationMessage('升级失败，请重试！')
-              }
             }
-          } else if (mode === '更新配置集') {
-            let data: any = {}
-            try {
-              let rootPath = path.join(getWorkspaceRoot(''), this.configUrl || 'config')
-              const dirs = fs.readdirSync(rootPath)
-              for (let i = 0; i < dirs.length; i++) {
-                const dir = dirs[i];
-                if (!(dir.charAt(0) === '.')) {
-                  let stat = fs.statSync(path.join(rootPath, dir))
-                  if (stat.isFile()) {
-                    const file = fs.readFileSync(path.join(rootPath, dir), 'utf-8')
-                    data[dir] = file
-                  }
-                }
-              }
-            } catch (error) {
+          } else {
+            window.showInformationMessage(`请在${this.configUrl}目录下放置配置文件`)
+          }
+        } else if (mode === '访问') {
+          res = await axios.get(`${this.url}/api/service/info?environmentId=${this.envId}&serviceId=${this.serviceName}`, {
+            headers: {
+              token: this.token
             }
-            if (Object.keys(data).length > 0) {
-              res = await axios.put(`${this.url}/api/environments/${this.envId}/configmaps/hs-screen`, {
-                environmentId: this.envId,
-                id: this.serviceName,
-                data: data,
-                currentVersion: '1'
-              }, {
-                headers: {
-                  token: this.token
-                }
-              })
-              if (res.data && res.data.code === '0') {
-                window.showInformationMessage('更新配置信息成功！')
-                this.cloudHandle('重启')
-              }
+          })
+          if (res.data && res.data.data && res.data.data.ingress && res.data.data.ingress[this.serviceName]) {
+            let cloudUrl = res.data.data.ingress[this.serviceName]
+            if (cloudUrl.includes('http')) {
+              open(cloudUrl)
             } else {
-              window.showInformationMessage(`请在${this.configUrl}目录下放置配置文件`)
+              open('http://' + cloudUrl)
             }
-          } else if (mode === '访问') {
-            res = await axios.get(`${this.url}/api/service/info?environmentId=${this.envId}&serviceId=${this.serviceName}`, {
-              headers: {
-                token: this.token
-              }
-            })
-            if (res.data && res.data.data && res.data.data.ingress && res.data.data.ingress[this.serviceName]) {
-              let cloudUrl = res.data.data.ingress[this.serviceName]
-              if (cloudUrl.includes('http')) {
-                open(cloudUrl)
-              } else {
-                open('http://' + cloudUrl)
-              }
-            } else {
-              window.showInformationMessage('请检查是否配置访问地址')
-            }
+          } else {
+            window.showInformationMessage('请检查是否配置访问地址')
           }
         }
       } else {
